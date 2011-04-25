3f3c0b402a5eb0bddd90d6dcddde94f1641a8da2
Jeff Casimir <jeff@jumpstartlab.com>
2011-04-25 11:20:24 -0500

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+#require 'openssl'
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+# Initialize the rails application
+JSContact::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JSContact::Application.config.secret_token = '91fc0b513488fcfc3650dcd86202414ab9a68dac64c062564077183501c554509ad4a4338e58444942e6dd2381eccc534cf757e110913756a1b8c9c94d636977'
